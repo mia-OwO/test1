a93945051c058a2f11d7e0b6b854398b0c617780
mikyung <Ruing.OqO@gmail.com>
2025-03-24 11:59:04 +0900

    출력문 추가

--- Car.java
+++ Car.java
@@ -1,5 +1,7 @@
 public class Car {
     public static void main(String[] args) {
         System.out.println("Car");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
     }
 }
